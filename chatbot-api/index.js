08a6f162cf4c56295f0d65c21f03d06ef49bb15e
Elisson Silva <elisson.asilva@mercadolivre.com.br>
2023-06-03 00:49:16 -0300

    feat: added iniciar

--- chatbot-api/index.js
+++ chatbot-api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const QRCode = require('qrcode-svg');
 const app = express();
+const steps = require('./steps');
 
 const client = new Client({
     authStrategy: new LocalAuth()
@@ -12,7 +13,28 @@ client.on('ready', () => {
 });
 
 client.on('message', (message) => {
-  console.log('Mensagem recebida:', message.body);
+
+  if(message.body === "iniciar") {
+    client.sendMessage(steps[0]['question'])
+  }
+  if(message.body === 2) {
+    client.sendMessage(steps[1]['question'])
+  }
+  if(message.body === 3) {
+    client.sendMessage(steps[2]['question'])
+  }
+  if(message.body === 4) {
+    client.sendMessage(steps[3]['question'])
+  }
+  if(message.body === 5) {
+    client.sendMessage(steps[4]['question'])
+  }
+  if(message.body === 6) {
+    client.sendMessage(steps[5]['question'])
+  }
+  if(message.body === 7) {
+    client.sendMessage(steps[6]['question'])
+  }
 });
 
 client.initialize();
@@ -26,6 +48,23 @@ app.get('/auth', (req, res) => {
     res.send(qrCodeSvg);
   });
 });
+
+app.get('/iniciar', (req, res) => {
+  const qr = new QRCode({
+    content: "https://api.whatsapp.com/send?phone=5592993806358&text=iniciar",
+    padding: 4,
+    width: 256,
+    height: 256,
+    color: '#000000',
+    background: '#ffffff',
+    ecl: 'M',
+  });
+  
+  // Gere o código SVG do QR code
+  const qrCodeSvg = qr.svg();
+  res.setHeader('Content-Type', 'image/svg+xml');
+  res.send(qrCodeSvg);
+})
 
 app.listen(3000, () => {
   console.log('Servidor iniciado na porta 3000');
---
+++ chatbot-api/steps/index.js
@@ -0,0 +1,105 @@
+const steps = [
+  {
+    id: 1,
+    question: `No império antigo da Babilônia,
+    Um líquido peculiar flui com harmonia.
+    Mas cuidado, ó habitantes da terra,
+    Um perigo oculto, uma triste guerra.
+    Beberão desse líquido vermelho,
+    Em suas taças, um veneno conselho.
+    Tão doce como mel, mas mortífero,
+    Um gole dele e lhe findará por inteiro.
+    Qual é esse liquido maldito, meu amigo?
+    Um enigma enigmático, um destino sombrio.
+    Pense bem antes de levantar a taça,
+    O desafio é descobrir qual é a ameaça.`,
+  },
+  {
+    id: 2,
+    question: `Uma força oculta paira no ar,
+    Um veneno que quer nos dominar.
+    Mas há um escudo, uma defesa sutil,
+    Contra o mal que nos faz fraquejar.
+    Não é físico, nem de aço ou ferro,
+    Mas uma armadura que nos mantém inteiros.
+    Nas páginas sagradas, seu poder se esconde,
+    Proteção contra o veneno dos monstros do além.
+    Leia e estude com atenção,
+    Encontre a resposta nessa lição.
+    Qual é a proteção contra esse veneno espiritual,
+    Que nos guia na jornada celestial?`
+  },
+  {
+    id: 3,
+    question: `Onde demônios residem e se ocultam,
+    Onde espíritos malignos dançam e relutam.
+    Nas sombras da noite, seu reino está,
+    Uma ferramenta de Satanás a enganar.
+    Palavras de medo, feitiçaria e aflição,
+    Usadas para semear a ilusão.
+    Indicam seu poder, sua vil trama,
+    Enganando almas, trazendo lama.
+    Nas entrelinhas, veja além do véu,
+    A resposta está lá, não se iluda, cruel.
+    Descubra o termo que revela a traição,
+    A atividade sombria de sua sedução.
+    Quais são essas expressões, então?
+    Um enigma ardiloso, uma questão de razão.
+    Pense bem e desvende esse mistério,
+    E proteja-se do engano e do cemitério.`
+  },
+  {
+    id: 4,
+    question: `Em tempos antigos, um engano se espalhou,
+    Na Babilônia, a ilusão se mostrou.
+    Um pilar das decepções, sutil e traiçoeiro,
+    Na ideia da morte, um falso tesoureiro.
+    A alma imortal, diziam, em sua crença,
+    Preparando o caminho para a influência.
+    Do espiritualismo, um poder maligno,
+    Uma armadilha ardilosa, um destino sinistro.
+    Desvende agora esse enigma ancestral,
+    A resposta está próxima, em tom celestial.
+    Qual é esse conceito distorcido, meu caro,
+    Que leva à influência prejudicial e amargo?
+    Reflete e decifra, com perspicácia e clareza,
+    Proteja-se do engano, da falsa natureza.
+    Pense e responda, revelando a verdade,
+    E escape da armadilha com sagacidade.`
+  },
+  {
+    id: 5,
+    question: `"Sou um nome que representa um imperador romano,
+    Comandei no século IV, Constantino é meu plano.
+    Mudanças e reformas fiz em meu reinado,
+    Um império fortalecido foi meu legado.
+    
+    Minha primeira sílaba é uma constante matemática,
+    Para descobrir meu nome, pense com tática.
+    Minha segunda sílaba tem gosto de café,
+    Encontrar minha resposta não é tão difícil, você vê.
+    
+    Quem sou eu, adivinhe, sem vacilar?
+    Um imperador famoso, o nome é Constantino, pode apostar!"`
+  },
+  {
+    id: 6,
+    question: `Grécia antiga, mas quem sou eu, uma divindade celeste?
+    Eu sou o brilho radiante que ilumina os dias, a fonte de calor e vida na Terra.
+    Adorado por muitos, eu testemunhei impérios crescerem e desaparecerem.
+    Embora minha adoração tenha diminuído ao longo dos séculos, meu poder permanece incontestável.
+    Quem sou eu?`
+  },
+  {
+    id: 7,
+    question: `O significado oculto que essa lei traz consigo, a revelar.
+    Aqui está um código secreto:
+    Pista 1: "Asas celestiais revelam o caminho."
+    Pista 2: "A chave reside no número do destino."
+    Pista 3: "O deslocamento é a chave para desvendar o código."
+    Código secreto: Dsrdoflsh 21
+    `
+  }
+]
+
+module.exports = steps;
