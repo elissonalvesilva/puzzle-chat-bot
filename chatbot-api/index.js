14f220a6c3206ca559d5f9fce5358eb51a031fa0
Elisson Silva <elisson.asilva@mercadolivre.com.br>
2023-06-02 21:53:28 -0300

    feat: added gitignore

--- chatbot-api/index.js
+++ chatbot-api/index.js
@@ -1,38 +1,20 @@
-const { Client } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
-const express = require('express');
+const { Client } = require('whatsapp-web.js');
 
 const client = new Client();
 
-const app = express();
+client.initialize();
 
-app.get('/', (req, res) => {
-  res.send('Chatbot do WhatsApp');
-});
-
-app.get('/qr', (req, res) => {
-  client.on('qr', (qr) => {
-    qrcode.generate(qr, { small: true });
-    res.send(qr);
-  });
-});
-
-app.listen(3000, () => {
-  console.log('Servidor rodando em http://localhost:3000');
-});
-
-client.on('message', (message) => {
-  if (message.body.toLowerCase() === 'oi') {
-    message.reply('Olá! Como posso ajudar?');
-  }
+client.on('qr', (qr) => {
+  qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
-  console.log('Aplicação pronta!');
+  console.log('Client is ready!');
 });
 
-client.on('auth_failure', (error) => {
-  console.error('Erro na autenticação:', error);
+client.on('message', (message) => {
+  if (message.body === 'hello') {
+    message.reply('Hiiiii');
+  }
 });
-
-client.initialize();
