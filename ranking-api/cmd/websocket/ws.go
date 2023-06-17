3892ff9ade5764d027aea446e5804d04357cc9cd
Elisson Silva <elisson.asilva@mercadolivre.com.br>
2023-06-16 23:37:57 -0300

    fix: fix connection

--- ranking-api/cmd/websocket/ws.go
+++ ranking-api/cmd/websocket/ws.go
@@ -1,12 +1,15 @@
 package websocket
 
 import (
+	"fmt"
+	"github.com/elissonalvesilva/puzzle-chat-bot/ranking-api/cmd/db"
 	"github.com/gorilla/websocket"
-	"log"
 	"net/http"
 )
 
 type WS struct {
+	clients map[*websocket.Conn]bool
+	db      *db.Database
 }
 
 var upgrader = websocket.Upgrader{
@@ -17,34 +20,32 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-func NewWebSocket() *WS {
-	return &WS{}
+func NewWebSocket(db *db.Database) *WS {
+	return &WS{
+		db: db,
+	}
 }
 
 func (ws *WS) WebsocketHandler(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
+	connection, _ := upgrader.Upgrade(w, r, nil)
 
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer conn.Close()
+	ws.clients[connection] = true // Save the connection using it as a key
 
-	// Agora você pode ler e escrever mensagens usando a conexão WebSocket
 	for {
-		messageType, message, err := conn.ReadMessage()
-		if err != nil {
-			log.Println(err)
-			break
+		mt, message, err := connection.ReadMessage()
+
+		if err != nil || mt == websocket.CloseMessage {
+			break // Exit the loop if the client tries to close the connection or the connection is interrupted
 		}
 
-		// Processar a mensagem recebida
-
-		// Enviar uma mensagem de volta para o cliente
-		err = conn.WriteMessage(messageType, message)
-		if err != nil {
-			log.Println(err)
-			break
-		}
+		go handleMessage(message)
 	}
+
+	delete(ws.clients, connection) // Removing the connection
+
+	connection.Close()
+}
+
+func handleMessage(message []byte) {
+	fmt.Println(string(message))
 }
