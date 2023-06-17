d26fbc0f9948ebf2155db9173bf72642b8528e39
Elisson Silva <elisson.asilva@mercadolivre.com.br>
2023-06-17 15:25:03 -0300

    feat: added cors

--- ranking-api/ws/main.go
+++ ranking-api/ws/main.go
@@ -4,7 +4,26 @@ import (
 	"github.com/gin-gonic/gin"
 	socketio "github.com/googollee/go-socket.io"
 	"log"
+	"net/http"
 )
+
+func GinMiddleware(allowOrigin string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", allowOrigin)
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, Content-Length, X-CSRF-Token, Token, session, Origin, Host, Connection, Accept-Encoding, Accept-Language, X-Requested-With")
+
+		if c.Request.Method == http.MethodOptions {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		c.Request.Header.Del("Origin")
+
+		c.Next()
+	}
+}
 
 func main() {
 	router := gin.Default()
@@ -45,6 +64,7 @@ func main() {
 	go server.Serve()
 	defer server.Close()
 
+	router.Use(GinMiddleware("http://localhost:3001"))
 	router.GET("/socket.io/*any", gin.WrapH(server))
 	router.POST("/socket.io/*any", gin.WrapH(server))
 
